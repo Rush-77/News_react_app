b4bf954fe5cf0513dc87e45c4af7d9f555b65722
rushikeshk <rushikeshk@cdac.in>
2023-06-20 17:18:19 +0530

    News populated using api

--- src/component/News.js
+++ src/component/News.js
@@ -49,19 +49,79 @@ export class News extends Component {
     super();
     this.state = {
       articles : this.articles,
-      loading : false
+      loading : false,
+      page : 1
     }
   }
+
+  async componentDidMount(){
+    console.log('inside componentDidMount' + this.state.page);
+    let newsApiUrl = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${this.state.page}&pageSize=18&apiKey=cba0f09a4f3544da9d9d821d621e6af5`;
+    let data = await fetch(newsApiUrl);
+    let parseData = await data.json();
+    this.setState({articles : parseData.articles,totalResults: parseData.totalResults});
+  }
  
+  // btnClick = async (stsFlag) =>{
+  //   if(stsFlag){
+  //     console.log('cal ' + Math.ceil(this.state.totalResults/18) + ' page ' + this.state.page);
+  //     if(this.state.page < Math.ceil(this.state.totalResults/18)){
+  //       console.log('inside page1 ' + this.state.page);
+  //       this.setState({page:this.state.page + 1});
+  //       console.log('inside page2 ' + this.state.page);
+  //     }
+  //     // console.log('page' + this.state.page);
+  //   }else{
+  //     // console.log('flag ' + stsFlag);
+  //     this.setState({page:this.state.page - 1});
+  //     console.log('page' + this.state.page);
+  //   }
+  //   console.log('page' + this.state.page);
+  //   let newsApiUrl = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${this.state.page}&pageSize=18&apiKey=cba0f09a4f3544da9d9d821d621e6af5`;
+  //   console.log(newsApiUrl);
+  //   let data = await fetch(newsApiUrl);
+  //   let parseData = await data.json();
+  //   this.setState({articles : parseData.articles});
+  // }
+
+  nextBtnClick = async() => {
+    console.log('page' + this.state.page);
+    if(this.state.page < Math.ceil(this.state.totalResults/18)){
+      let newsApiUrl = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${this.state.page + 1}&pageSize=18&apiKey=cba0f09a4f3544da9d9d821d621e6af5`;
+      let data = await fetch(newsApiUrl);
+      let parseData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles : parseData.articles
+      });
+    }
+  }
   
+   prevBtnClick= async() =>{
+    console.log('page' + this.state.page);
+    let newsApiUrl = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${this.state.page - 1}&pageSize=18&apiKey=cba0f09a4f3544da9d9d821d621e6af5`;
+    let data = await fetch(newsApiUrl);
+    let parseData = await data.json();
+    this.setState({page: this.state.page - 1,articles : parseData.articles});
+  }
+
   render() {
     return (
       <div className='container my-4'>
-        <h1>Today's News</h1>
-        <div className="row">
+        <h1>Today's Business News</h1>
+        <div className="row" >
           {this.state.articles.map((ele)=>{
-            return <NewsItem className='col-md-4' title={ele.title} description={ele.description} url={ele.url} urlToImage={ele.urlToImage} />
+            return <div className="col-md-4" key={ele.url}>
+                    <NewsItem  className='col-md-4' title={ele.title} description={ele.description} url={ele.url} urlToImage={ele.urlToImage} />
+                  </div>
           })}
+        </div>
+        <div className="container d-flex justify-content-between my-2">
+          {/* <button type="button" className="btn btn-dark" disabled={this.state.page<= 1} onClick={() => this.btnClick(0)}>Previous</button>
+          <button type="button" className="btn btn-dark" onClick={() => this.btnClick(1)}>Next</button> */}
+
+          <button type="button" className="btn btn-dark" disabled={this.state.page<= 1} onClick={this.prevBtnClick}>Previous</button>
+          <button type="button" className="btn btn-dark" disabled={this.state.page >= Math.ceil(this.state.totalResults/18)} onClick={this.nextBtnClick}>Next</button>
         </div>
       </div>
     )
